146239907f907a81641d14541cf89c8267db9d0a
VolodumurGruv <gru.v@i.ua>
2022-07-16 15:41:08 +0300

    lesson 7 done

---
+++ BasicJS_FE3_2_Volodymyr_Grushka/module_3_lesson_7.js
@@ -0,0 +1,43 @@
+const obj = {
+	name: "Jim",
+	age: 77,
+	city: "London",
+	id: 3,
+};
+
+console.log(Object.keys(obj));
+
+obj.language = "English";
+
+console.log(Object.values(obj));
+
+console.log(Object.keys(obj).length);
+console.log(Object.entries(obj));
+
+for (const key in obj) {
+	console.log(`${key}: ${obj[key]}`);
+}
+
+const newObj = {};
+
+for (const key in obj) {
+	newObj[key] = obj[key];
+}
+
+console.log(newObj);
+console.log(Object.assign({}, obj));
+
+newObj.simpleFun = () => {
+	console.log("Simple function");
+};
+
+newObj.simpleFun();
+
+Object.freeze(newObj);
+
+newObj.id = 2;
+newObj.surname = "Bob";
+
+console.log(newObj);
+
+console.log("surname" in newObj);
