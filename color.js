b3effe94640015aef42a9f04425fed9899ad910f
VolodumurGrushka <gru.v@i.ua>
2022-07-25 00:17:07 +0300

    changed picking up color

--- color.js
+++ color.js
@@ -1,9 +1,12 @@
-const colorPanel = document.querySelector(".colors");
+const r = document.querySelector("#r");
+const g = document.querySelector("#g");
+const b = document.querySelector("#b");
+const color = document.querySelector(".color");
 const drawingField = document.querySelector(".drawing-field");
 const brushShape = document.querySelector(".brush");
 const width = document.querySelector(".horizontal");
 const height = document.querySelector(".vertical");
-let color = "";
+
 let shape = "square";
 
 width.max = 100;
@@ -11,9 +14,22 @@ width.min = 1;
 height.max = 100;
 height.min = 1;
 
-colorPanel.addEventListener("click", (event) => {
-	event.stopPropagation();
-	color = event.path[0].classList.value;
+let rgb = `rgb(${r.value}, ${g.value}, ${b.value})`;
+console.log(color.style.backgroundColor);
+color.style.backgroundColor = `rgb(${r.value}, ${g.value}, ${b.value})`;
+console.log(color.style.backgroundColor);
+
+r.addEventListener("change", () => {
+	rgb = `rgb(${r.value}, ${g.value}, ${b.value})`;
+	color.style.backgroundColor = rgb;
+});
+g.addEventListener("change", () => {
+	rgb = `rgb(${r.value}, ${g.value}, ${b.value})`;
+	color.style.backgroundColor = rgb;
+});
+b.addEventListener("change", () => {
+	rgb = `rgb(${r.value}, ${g.value}, ${b.value})`;
+	color.style.backgroundColor = rgb;
 });
 
 brushShape.addEventListener("click", (event) => {
@@ -22,12 +38,12 @@ brushShape.addEventListener("click", (event) => {
 
 drawingField.addEventListener("mouseup", (event) => {
 	const el = document.createElement("div");
-
+	console.log(rgb);
 	el.classList.add(shape);
 	el.style.width = width.value + "px";
 	el.style.height = height.value + "px";
 	el.style.border = "none";
-	el.style.backgroundColor = color;
+	el.style.backgroundColor = rgb;
 	el.style.display = "block";
 	el.style.position = "absolute";
 	el.style.top = event.y + "px";
